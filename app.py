e0547fc45daeb93b71c9d5e1ef4b7f23fd8f5f8e
jose <joseospinahiguita4474@gmail.com>
2023-08-26 16:59:07 -0500

    26/08/23

--- app.py
+++ app.py
@@ -1,44 +1,70 @@
-def ejercicio1():
-    # Implementa aquí el código para el ejercicio 1
+def ejercicio_1():
+    numero = int(input("Ingresa un número: "))
+    if numero % 2 == 0:
+        print("El número es par.")
+    else:
+        print("El número es impar.")
 
-def ejercicio2():
-    # Implementa aquí el código para el ejercicio 2
+def ejercicio_2():
+    limite = int(input("Ingresa un número límite: "))
+    suma = 0
+    for i in range(1, limite + 1):
+        suma += i
+    print("La suma de los números del 1 al", limite, "es:", suma)
 
-def ejercicio3():
-    # Implementa aquí el código para el ejercicio 3
+def ejercicio_3():
+    num = int(input("Ingresa un número: "))
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    print("El factorial de", num, "es:", factorial)
 
-def ejercicio4():
-    # Implementa aquí el código para el ejercicio 4
+def ejercicio_4():
+    base = float(input("Ingresa la base: "))
+    exponente = int(input("Ingresa el exponente: "))
+    resultado = 1
+    for _ in range(exponente):
+        resultado *= base
+    print(base, "elevado a la", exponente, "es:", resultado)
 
-def ejercicio5():
-    # Implementa aquí el código para el ejercicio 5
+def ejercicio_5():
+    while True:
+        contraseña = input("Ingresa la contraseña (debe tener al menos 8 caracteres): ")
+        if len(contraseña) >= 8:
+            print("Contraseña válida. ¡Registro exitoso!")
+            break
+        else:
+            print("La contraseña es demasiado corta. Inténtalo nuevamente.")
 
 def main():
     while True:
-        print("Menú:")
-        print("1. Ejercicio 1")
-        print("2. Ejercicio 2")
-        print("3. Ejercicio 3")
-        print("4. Ejercicio 4")
-        print("5. Ejercicio 5")
+        print("Menú de ejercicios:")
+        print("1. Par o impar")
+        print("2. Suma de números")
+        print("3. Factorial")
+        print("4. Potencia")
+        print("5. Registro con contraseña")
         print("6. Salir")
-
-        opcion = input("Selecciona una opción: ")
-
-        if opcion == "1":
-            ejercicio1()
-        elif opcion == "2":
-            ejercicio2()
-        elif opcion == "3":
-            ejercicio3()
-        elif opcion == "4":
-            ejercicio4()
-        elif opcion == "5":
-            ejercicio5()
-        elif opcion == "6":
+        
+        opcion = int(input("Selecciona una opción: "))
+        
+        if opcion == 6:
+            print("¡Hasta luego!")
             break
+        elif opcion == 1:
+            ejercicio_1()
+        elif opcion == 2:
+            ejercicio_2()
+        elif opcion == 3:
+            ejercicio_3()
+        elif opcion == 4:
+            ejercicio_4()
+        elif opcion == 5:
+            ejercicio_5()
         else:
-            print("Opción inválida. Por favor, selecciona una opción válida.")
+            print("Opción inválida. Selecciona nuevamente.")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
+
+
